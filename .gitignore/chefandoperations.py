9b62d7dd4e3f493787ecee3e5ff35a595b44e3d9
bhuveshgautam <42464227+bhuveshgautam@users.noreply.github.com>
2018-10-14 15:38:30 +0530

    Create chefandoperations.py

---
+++ .gitignore/chefandoperations.py
@@ -0,0 +1,23 @@
+t=int(input())
+for i in range(t):
+    arrlen=int(input())
+    arr1=input().split()
+    arr2=input().split()
+    for i in range(arrlen):
+        arr1[i]=int(arr1[i])
+    for i in range(arrlen):
+        arr2[i]=int(arr2[i])
+    ctr=0
+    for i in range(arrlen):
+        for j in range(i,arrlen):
+            k=1
+            arr1[j]=arr1[j]+k
+            k=k+1
+        if(arr1==arr2):
+            ctr=1
+            break
+        
+    if(ctr==1):
+        print("TAK")
+    if(ctr==0):
+        print("NIE")
