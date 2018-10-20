9d5984ddd9ac1c0b9ce36e00d835d8731cb253dd
Chirag Gupta <42840488+chirag2506@users.noreply.github.com>
2018-10-20 13:12:07 +0530

    Update card.py

--- .gitignore/card.py
+++ .gitignore/card.py
@@ -1,20 +1,24 @@
 test = int(input())
 for n in range(test):
-    ctr=0
+    ctr = 0
     N = int(input())
     cards = input().split()
     
-    
     for x in range(N):
         cards[x] = int(cards[x])
-    i=1
-        
-    while(i<N and ctr<2):
-        if(cards[i]<cards[i-1]):
-            ctr=ctr+1
-        i=i+1
+    
+    i = 1   
+    while(i < N and ctr < 2):
+        if(cards[i] < cards[i - 1]):
+            ctr = ctr + 1
+        i = i + 1
             
-    if(ctr<=1):
+    if(ctr == 0):
         print('YES')
-    else:
+    elif(ctr == 1):
+        if(cards[0] >= cards[N - 1]):
+            print('YES')
+        else:
+            print('NO')
+    else:        
         print('NO')
